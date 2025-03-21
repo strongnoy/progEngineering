e87f696167186383744e7a491e71ccc72fda597e
strongnoy <damir.faridonov@mail.ru>
2025-03-21 21:08:55 +0500

    little fixes

--- image_detection/image_detection.py
+++ image_detection/image_detection.py
@@ -7,24 +7,20 @@ from io import BytesIO
 
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
 model.eval()
-url = "https://c.pxhere.com/photos/20/52/photo-47141.jpg!d"  # Вставьте ссылку на ваше изображение
+url = "https://c.pxhere.com/photos/20/52/photo-47141.jpg!d"
 response = requests.get(url)
 img = Image.open(BytesIO(response.content))
 
-# Применяем модель для обнаружения объектов на изображении
 results = model(img)
 
-# Показываем результат
-results.show()  # Показывает изображение с отмеченными объектами
-# Получаем детекции
-predictions = results.xywh[0]  # Координаты ограничивающих рамок
+results.show()
+predictions = results.xywh[0]
 print(predictions)
 
-# Получаем имена классов объектов
+
 labels = results.names
 print(labels)
 
-# Получаем метки классов для каждого объекта
 class_ids = predictions[:, -1].tolist()
 for class_id in class_ids:
     print(labels[int(class_id)])
